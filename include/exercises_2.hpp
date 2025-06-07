243425aa0192f66b66c665de4f6db5389c47c0d7
Kaio Felipe <kaiofelipe2005@gmail.com>
2025-06-06 22:22:30 -0300

    feat: add functions signatures from list 5 exercises

---
+++ include/exercises_2.hpp
@@ -0,0 +1,46 @@
+#include <list>
+#include <string>
+#include <span>
+
+namespace ex2
+{
+
+/// Retorna a soma de todos os elementos de um vector de tamanho n
+/// que estão em posições pares.
+int sum(const std::span<int>& vec);
+
+/// Retorna o produto de todos os elementos de uma list de início em it.
+int product(const std::list<int>& lst, std::list<int>::iterator it);
+
+/// Retorna o menor dos elementos de um vector de tamanho n.
+int min(const std::span<const int>& vec);
+
+/// Retorna o menor dos elementos de uma lista de início em it.
+int min(const std::list<int>& lst, std::list<int>::iterator it);
+
+/// Informa se um elemento está em um vector de tamanho n.
+bool find(const std::span<const int>& vec, const int& elem);
+
+/// Retorna a quantidade de ocorrência de um elemento em um vector de
+/// tamanho n.
+int count(const std::span<const int>& vec, const int& elem);
+
+// `span` é como uma referência para um vetor ou um array (de c ou c++).
+// como não copia dados, não há necessidade de passar por referência, ainda mais
+// se o objetivo for modificar os elementos que acessíveis por meio do span.
+
+/// Inverte os elementos em um vector, de início em first e final em last.
+void reverse(std::span<int> vec, size_t first, size_t last);
+
+/// Inverte os elementos em uma list, de início em first e final em last.
+void reverse(std::list<int>& lst, std::list<int>::iterator first, std::list<int>::iterator last);
+
+/// Verifica se uma string forma um palíndromo.
+bool palindrome(const std::string& str, const size_t first, const size_t last);
+bool palindrome(const std::string& str);
+
+/// Verifica se os elementos de uma list foram um palíndromo.
+bool palindrome(const std::list<int>& lst, std::list<int>::iterator first, const std::list<int>::iterator last);
+bool palindrome(const std::list<int>& lst);
+
+}
---
+++ tests/exercises_2_test.cpp
@@ -0,0 +1,86 @@
+#include "exercises_2.hpp"
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("`sum` deve somar todos os valores em posições pares de um vetor de inteiros")
+{
+    auto vec = std::vector<int>({1, 45, 6, 0, 184, 583, 804, 100, -394});
+    REQUIRE(334 == ex2::sum(vec));
+}
+
+TEST_CASE("`product` deveria retornar o produto de todos os elementos de uma lista a partir de um ponto")
+{
+    auto list = std::list {1, 5, 0, 30, 20, 40, 58, 80, 19000};
+    auto list_iterator = list.begin();
+
+    // posiciona o início da iteração no elemento 30
+    std::advance(list_iterator, 2);
+
+    REQUIRE(2115840000000 == ex2::product(list, list_iterator));
+}
+
+TEST_CASE("`min` deveria encontrar o menor elemento de um array-like")
+{
+    REQUIRE(-500 == ex2::min(std::vector<int>({-100, 0, 10, 50, 400, 1800, 9483, -499, -500, 399, -20, 0})));
+}
+
+TEST_CASE("`min` deveria encontrar o menor elemento em uma lista a partir de um dado elemento")
+{
+    auto list = std::list {-500, 0, 500, -200, 490, 900, -305};
+    REQUIRE(-500 == ex2::min(list, list.begin()));
+    REQUIRE(-305 == ex2::min(list, std::next(list.begin())));
+}
+
+TEST_CASE("`find` deveria informar se um elemento existe em um array-like")
+{
+    int arr[] = {400, 2904, -394298, 482, 204831, 401894, 37983};
+
+    REQUIRE_FALSE(ex2::find(arr, 0));
+    REQUIRE(ex2::find(arr, 2904));
+}
+
+TEST_CASE("`count` deveria contar a ocorrência de um elemento em um array-like")
+{
+    int arr[] = {4900, 5498, 290, 984, 290, 1940, 4900, 10, 290, 25};
+
+    REQUIRE(1 == ex2::count(arr, 10));
+    REQUIRE(2 == ex2::count(arr, 4900));
+    REQUIRE(3 == ex2::count(arr, 290));
+}
+
+TEST_CASE("`reverse` deveria inverter um segmento de um array-like")
+{
+    int arr[] ={1, 2, 3, 7, 6, 5, 4, 8, 9, 10};
+    ex2::reverse(arr, (size_t) 3, (size_t) 6);
+    
+    int expected[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    REQUIRE(expected == arr);
+}
+
+TEST_CASE("`reverse` deveria inverter uma lista no segmento específicado")
+{
+    auto list = std::list<int> {1, 2, 3, 7, 6, 5, 4, 8, 9, 10};
+    auto expected = std::list<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    ex2::reverse(list, std::next(list.begin(), 3), std::next(list.begin(), 6));
+    REQUIRE(expected == list);
+}
+
+TEST_CASE("`palindrome` deveria verificar se uma string é um palíndromo")
+{
+    REQUIRE(ex2::palindrome("arara"));
+    REQUIRE(ex2::palindrome("ararra"));
+    REQUIRE_FALSE(ex2::palindrome("carro"));
+}
+
+TEST_CASE("`palindrome` deveria verificar se os elementos de uma lista formam um palíndromo")
+{
+    auto palindrome_list = std::list<int> {1, 2, 3, 4, 5, 4, 3, 2, 1};
+    auto palindrome_list_2 = std::list<int> {1, 2, 3, 4, 5, 5, 4, 3, 2, 1};
+    auto non_palindrome_list = std::list<int> {1, 2, 3, 4, 5, 5, 3, 2, 1};
+    auto non_palindrome_list_2 = std::list<int> {1, 2, 3, 4, 5, 3, 2, 1};
+
+    REQUIRE(ex2::palindrome(palindrome_list));
+    REQUIRE(ex2::palindrome(palindrome_list_2));
+    REQUIRE_FALSE(ex2::palindrome(non_palindrome_list));
+    REQUIRE_FALSE(ex2::palindrome(non_palindrome_list_2));
+}
